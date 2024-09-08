e1bcf00813a2629bb14ae72af98229d421d236ab
Varshitha y s <86124941+Varshithays@users.noreply.github.com>
2024-09-08 17:29:44 +0530

    Added new code
    with time and space complexity

--- Reverseinplace.java
+++ Reverseinplace.java
@@ -23,3 +23,35 @@ public class Reverseinplace {
             reverse(arr, n);
     }
 }
+/* Reverse an Array with different approaches 1st is with new Array and other Swapping*/
+import java.util.Arrays;
+
+public class ReverseArray {
+//Reverse the given array different approaches
+    public static void main(String... reverseArray){
+        int[] array={2,3,4,5,1,6,7,8,0};
+        int length=array.length;
+//FIRST APPROACH WITH NEW ARRAY
+        System.out.println(length);
+        int[] newArray = new int[length];
+        for(int i= length-1,j=0;i>=0;i--,j++){
+            newArray[j]=array[i];
+        }
+        //Space complexity: n+n =2n
+        //Time Complexity : n depend on length of array
+        System.out.println(Arrays.toString(newArray));
+
+        //2 SWAPPING
+        for(int i=0;i<length/2;i++){
+            int temp=array[i];
+            System.out.println(length);
+            array[i]=array[length-(i+1)];
+            array[length-(i+1)]=temp;
+
+        }
+        //Space Complexity :n+1 = 2n
+        //Time Complexity: n/2
+        System.out.println(Arrays.toString(array));
+    }
+
+}
